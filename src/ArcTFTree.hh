bf33e0f7541ea6ea7faa46112e967717fd71e81a
Howard <makubex49@gmail.com>
2024-04-08 10:00:58 +0800

    add arc tf tree tool

---
+++ src/ArcTFTree.hh
@@ -0,0 +1,126 @@
+﻿#ifndef ARC_TF_TREE_NODE_H_
+#define ARC_TF_TREE_NODE_H_
+
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+namespace ARC_TF
+{
+    struct TreeNode {
+        std::string value;
+        std::vector<TreeNode*> children;
+        TreeNode* parent;
+    };
+
+    class Tree {
+    private:
+        std::map<std::string, TreeNode*> nodes; // Tree的本體
+
+    public:
+        Tree() {}
+        void removeChild(const std::string& parentValue, const std::string& childValue)
+        {
+            auto parentIt = nodes.find(parentValue);
+            auto childIt = nodes.find(childValue);
+
+            // 確保父節點和子節點都存在
+            if (parentIt == nodes.end() || childIt == nodes.end()) {
+                return;
+            }
+            auto& children = parentIt->second->children;
+            auto childToRemove = childIt->second;
+
+            auto it = std::find(children.begin(), children.end(), childToRemove);
+            nodes[parentValue]->children.erase(it);
+        }
+
+        void addChild(const std::string& parentValue, const std::string& childValue)
+        {
+            TreeNode* parent = nullptr;
+            if (nodes.find(parentValue) == nodes.end())
+            {
+                parent = new TreeNode();
+                parent->value = parentValue;
+                nodes[parentValue] = parent;
+            }
+            else {
+                parent = nodes[parentValue];
+            }
+
+            TreeNode* child = nullptr;
+
+            if (nodes.find(childValue) != nodes.end())
+            {
+                child = nodes[childValue];
+                if (child->parent != nullptr && child->parent != parent)
+                {
+                    this->removeChild(child->parent->value, childValue);
+                }
+            }
+            else
+            {
+                child = new TreeNode();
+                child->value = childValue;
+                nodes[childValue] = child;
+            }
+
+            child->parent = parent; // 設置父節點的指標
+            parent->children.push_back(child); // 改為使用指標存取
+        }
+
+        std::vector<TreeNode*> getRoots()
+        {
+            std::vector<TreeNode*> roots;
+            for (const auto& pair : nodes)
+            {
+                if (pair.second->parent == nullptr)
+                {
+                    roots.push_back(pair.second);
+                }
+            }
+            return roots;
+        }
+
+        // 將樹狀結構轉換為字串
+        std::string toString()
+        {
+            std::stringstream ss;
+
+            auto roots = getRoots();
+            for (const auto root : roots)
+            {
+                treeToStringHelper(root, ss);
+            }
+
+            return ss.str();
+        }
+
+        // 程式結束時釋放動態配置的記憶體
+        ~Tree() {
+            for (auto& pair : nodes)
+            {
+                delete pair.second;
+            }
+        }
+
+    private:
+        void treeToStringHelper(TreeNode* node, std::stringstream& ss, int level = 0)
+        {
+            for (int i = 0; i < level; ++i)
+            {
+                ss << "  ";
+            }
+            ss << "|-" << node->value << std::endl;
+
+            for (const auto& child : node->children)
+            {
+                treeToStringHelper(child, ss, level + 1);
+            }
+        }
+    };
+}
+
+#endif 
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,12 @@
+#include "ArcTFTree.hh"
+
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
 #include <yaml-cpp/yaml.h>
 #include <QDebug>
+
+#define ROS_PRINT(...) RCLCPP_INFO(this->node_->get_logger(), __VA_ARGS__)
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
@@ -54,30 +58,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-class TFTreeNode {
-public:
-    std::string tf_name;
-    std::vector<std::shared_ptr<TFTreeNode>> children;
-    std::shared_ptr<TFTreeNode> parent;
-
-    explicit TFTreeNode(std::string val, TFTreeNode* parent) : tf_name(val) {}
-
-    void addChild(const std::shared_ptr<TFTreeNode>& child) {
-        children.push_back(child);
-    }
-
-    void display(int level = 0) {
-        for (int i = 0; i < level; ++i) {
-            std::cout << "  ";
-        }
-        std::cout << "|-" << tf_name << std::endl;
-
-        for (const auto& child : children) {
-            child->display(level + 1);
-        }
-    }
-};
-
 void MainWindow::initSpin(void)
 {
     // Initialize spinning with a timer interval of 1 ms
@@ -122,57 +102,22 @@ void MainWindow::on_pushButton_clicked()
     // 打印 YAML 字串
 //    std::cout << "YAML Node Content:\n" << emitter.c_str() << std::endl;
 
-    RCLCPP_INFO(node_->get_logger(), "TF Struct---");
+    ROS_PRINT("TF Struct---");
+
+    ARC_TF::Tree tf_tree;
     std::map<std::string, std::string> tf_parent_map;
     for (const auto& pair : yaml_node)
     {
         std::string tf_name = pair.first.as<std::string>();
         YAML::Node value_node = pair.second;
+        std::string tf_parent = value_node["parent"].as<std::string>();
 
-        std::string parent = value_node["parent"].as<std::string>();
-
-//        std::cout << "Key: " << key << ", Value: " << value_node << std::endl;
-        RCLCPP_INFO(node_->get_logger(), "%s -> %s", parent.c_str(), tf_name.c_str() );
-        tf_parent_map[tf_name] = parent;
+        tf_tree.addChild(tf_parent, tf_name);
     }
 
-//    this->ui->treeWidget->setHeaderLabels({"TF Tree"});
+    ROS_PRINT("\r\n%s", tf_tree.toString().c_str());
 
-//    // 創建根節點
-//    QTreeWidgetItem *rootItem = new QTreeWidgetItem(this->ui->treeWidget);
-//    rootItem->setText(0, "Root");
+//    std::cout << tree.toString() << std::endl;
 
-//    // 添加子節點到根節點
-//    QTreeWidgetItem *itemA = new QTreeWidgetItem(rootItem);
-//    itemA->setText(0, "Item A");
 
-//    QTreeWidgetItem *itemB = new QTreeWidgetItem(rootItem);
-//    itemB->setText(0, "Item B");
-
-//    // 添加子節點到 Item A
-//    QTreeWidgetItem *subItemA = new QTreeWidgetItem(itemA);
-//    subItemA->setText(0, "SubItem A");
-
-//    // 將根節點設置到 QTreeWidget 中
-//    this->ui->treeWidget->addTopLevelItem(rootItem);
-//    this->ui->treeWidget->addTopLevelItem(rootItem);
-
-//    // 設置 QTreeWidget 的樣式和大小
-//    this->ui->treeWidget->setWindowTitle("Simple TreeWidget Example");
-
-    // 創建根節點
-    auto root = std::make_shared<TFTreeNode>(1);
-
-    // 添加子節點到根節點
-    auto child1 = std::make_shared<TFTreeNode>(2);
-    auto child2 = std::make_shared<TFTreeNode>(3);
-    root->addChild(child1);
-    root->addChild(child2);
-
-    // 添加子節點到子節點
-    auto subChild1 = std::make_shared<TFTreeNode>(4);
-    child1->addChild(subChild1);
-
-    // 顯示樹狀結構
-    root->display();
 }
